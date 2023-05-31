3f3615cac49ab55179c00d61b7f5c4e782486360
Vicente Aguirre <vicenteignacioac22@gmail.com>
2023-05-31 05:16:51 -0400

    parte 2 del desafio

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const { agregarPosts, obtenerPosts } = require("./consultas.js");
+const {
+  agregarPosts,
+  obtenerPosts,
+  agregarLike,
+  borrarPosts,
+} = require("./consultas.js");
 const cors = require("cors");
 
 app.use(morgan("dev"));
@@ -11,14 +16,41 @@ app.use(cors());
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
 });
-
+// Parte 1 Desafio LikeMe
+// obtenerPosts = GET /posts
 app.get("/posts", async (req, res) => {
   const posts = await obtenerPosts();
-  res.send(posts);
+  res.json(posts);
+});
+// agregarPost = POST /posts
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, img, descripcion, likes } = req.body;
+    const post = await agregarPosts(titulo, img, descripcion, likes);
+    res.json({titulo, img, descripcion, likes});
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.post("/posts", async (req, res) => {
-  const { titulo, img, descripcion, likes } = req.body;
-  const post = await agregarPosts(titulo, img, descripcion, likes);
-  res.send(post);
+// Parte 2 Desafio LikeMe
+// agregarLike = PUT /like
+app.put("/posts/like/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const like = await agregarLike(id);
+    res.send(like);
+  } catch (error) {
+    console.error(error);
+  }
+});
+// borrarPost = DELETE /posts
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await borrarPosts(id);
+    res.send(post);
+  } catch (error) {
+    console.error(error);
+  }
 });
